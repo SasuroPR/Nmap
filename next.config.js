b8ca0b593bf43a3dabe38ac93db1189a5742eacf
SasuroPR <144629453+SasuroPR@users.noreply.github.com>
2023-10-27 14:43:49 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+    domains: ["avatars.githubusercontent.com"],
+  },
+};
+
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        nav: "80%",
+      },
+      colors: {
+        primary: colors.sky,
+      },
+      backgroundColor: {
+        light: colors.gray["50"],
+        dark: "#1f2224",
+      },
+      textColor: {
+        titles: {
+          light: colors.zinc["900"],
+          dark: colors.zinc["100"],
+        },
+        subtitles: {
+          light: colors.zinc["700"],
+          dark: colors.zinc["100"],
+        },
+        descriptions: {
+          light: colors.zinc["500"],
+          dark: colors.zinc["500"],
+        },
+        bases: {
+          light: colors.zinc["600"],
+          dark: colors.zinc["400"],
+        },
+      },
+      borderColor: {
+        base: {
+          light: colors.gray["300"],
+          dark: colors.zinc["600"],
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+  darkMode: "class",
+};
